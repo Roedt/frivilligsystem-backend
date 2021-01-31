8fa197ec2a79d423f334bb76ed221ab00a139ef4
Mads Opheim <mads@fostad.net>
2021-01-31 23:05:10 +0100

    #1: Forenkalr bort interface

--- src/main/kotlin/no/roedt/frivilligsystem/hypersys/HypersysService.kt
+++ src/main/kotlin/no/roedt/frivilligsystem/hypersys/HypersysService.kt
@@ -1,24 +1,15 @@
 package no.roedt.frivilligsystem.hypersys
 
-import no.roedt.frivilligsystem.hypersys.externalModel.Organisasjonsledd
-import no.roedt.frivilligsystem.hypersys.externalModel.Organs
-import no.roedt.frivilligsystem.hypersys.externalModel.SingleOrgan
-import org.eclipse.microprofile.jwt.JsonWebToken
 import javax.enterprise.context.ApplicationScoped
-import javax.inject.Inject
-
-interface HypersysService {
-    fun login(loginRequest: LoginRequest): Token
-}
 
 @ApplicationScoped
-class HypersysServiceBean(
+class HypersysService(
     val hypersysProxy: HypersysProxy,
     val hypersysSystemTokenVerifier: HypersysSystemTokenVerifier,
     val hypersysLoginBean: HypersysLoginBean
-) : HypersysService {
+) {
 
-    override fun login(loginRequest: LoginRequest): Token = hypersysLoginBean.login(loginRequest)
+    fun login(loginRequest: LoginRequest): Token = hypersysLoginBean.login(loginRequest)
 
 
     private fun getSystemToken() = hypersysSystemTokenVerifier.assertGyldigSystemToken()
